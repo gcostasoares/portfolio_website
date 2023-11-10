2d1456eb14e3870cb01d5a7e6a50e7b48730c0fb
Gabriel Costa Soares <119671456+gcostasoares@users.noreply.github.com>
2023-11-10 17:14:06 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -90,35 +90,6 @@ navItems.forEach((item) => {
     }
   });
 });
-
-const form = document.getElementById('kontakt');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  checkInputs();
-});
-
-function checkInputs() {
-  const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-
-  if (usernameValue === '') {
-    setErrorFor(username, 'Name ist ungültig');
-  } else {
-    setSuccessFor(username);
-  }
-
-  if (emailValue === '') {
-    setErrorFor(email, 'Emailadresse ist ungültig');
-  } else if (!isEmail(emailValue)) {
-    setErrorFor(email, 'Not a valid email');
-  } else {
-    setSuccessFor(email);
-  }
-}
-
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
   const small = formControl.querySelector('small');
@@ -129,11 +100,79 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
+  messageValidation = true;
 }
 
 function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/.test(email);
 }
+
+
+const form = document.getElementById('kontakt');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  checkInputs();
+});
+
+let nameValidation = false;
+let emailValidation = false;
+let messageValidation = false;
+
+function checkInputs() {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+
+  if (usernameValue === '') {
+    setErrorFor(username, 'Name ist ungültig');
+  } else {
+    setSuccessFor(username);
+    nameValidation = true;
+  }
+
+  if (emailValue === '') {
+    setErrorFor(email, 'Emailadresse ist ungültig');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Not a valid email');
+  } else {
+    setSuccessFor(email);
+    emailValidation = true;
+  }
+
+
+  if (nameValidation && emailValidation && messageValidation) {
+    sendEmail();
+    function sendEmail() {
+      var name = document.getElementById('username').value;
+      var email = document.getElementById('email').value;
+      var message = document.querySelector('.message textarea').value;
+      var messageSuccess = document.getElementById('message-success');
+      var messageError = document.getElementById('message-error');
+    
+      emailjs.send("service_8ydndpo", "template_kwpvdnm", {
+        from_name: name,
+        from_email: email,
+        message_textarea: message
+      }).then(
+        function(response) {
+          console.log("Email sent successfully:", response);
+          kontakt.style.display = 'none';
+          messageSuccess.style.display = 'flex';
+        },
+        function(error) {
+          console.log("Email failed to send:", error);
+          messageError.style.display = 'flex';
+        }
+      );
+    }
+  }
+}
+
+
+
+
 
 function changeBackgroundColor(color) {
   document.body.style.backgroundColor = color;
@@ -167,26 +206,4 @@ for (var i = 0; i < logoElementsOnLoad.length; i++) {
   logoElementsOnLoad[i].style.pointerEvents = "none";
 }
 
-function sendEmail(event) {
-  event.preventDefault();
 
-
-  var name = document.getElementById('username').value;
-  var email = document.getElementById('email').value;
-  var message = document.querySelector('.message textarea').value;
-
-  emailjs.send("service_8ydndpo", "template_kwpvdnm", {
-    from_name: name,
-    from_email: email,
-    message_textarea: message
-  }).then(
-    function(response) {
-      console.log("Email sent successfully:", response);
-
-    },
-    function(error) {
-      console.log("Email failed to send:", error);
-     
-    }
-  );
-}
