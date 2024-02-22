776af74026fe6d18f915dab950d30013e5422c54
Gabriel Costa Soares <119671456+gcostasoares@users.noreply.github.com>
2024-02-22 17:20:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
+const header = document.querySelector('.header');
+let lastScrollTop = 0;
+let scrolled = false;
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+
+        header.classList.remove('fixed');
+        if (!scrolled) {
+            header.style.visibility = 'hidden';
+        }
+    } else if (scrollTop < lastScrollTop && !scrolled) {
+     
+        header.classList.add('fixed');
+        header.style.visibility = 'visible';
+    }
+
+    if (scrollTop === 0) {
+     
+        scrolled = false;
+    }
+
+    lastScrollTop = scrollTop;
+});
+
+
+
 const burgerElement = document.querySelector('.burger');
-const navElement = document.querySelector('.overlayer'); // Corrected class name
+const navElement = document.querySelector('.overlayer'); 
+const navContainer = document.querySelector('.burger__nav__container')
 
 burgerElement.addEventListener('click', function() {
+  const isExpanded = burgerElement.getAttribute('aria-expanded') === 'true'; 
+  burgerElement.setAttribute('aria-expanded', !isExpanded);
+  navElement.style.left = isExpanded ? '100vw' : '0';
+});
+
+navContainer.addEventListener('click', function() {
   const isExpanded = burgerElement.getAttribute('aria-expanded') === 'true'; 
   burgerElement.setAttribute('aria-expanded', !isExpanded);
   navElement.style.left = isExpanded ? '100vw' : '0';
@@ -25,7 +61,7 @@ function textSizeFunction() {
 
     var textSizeRatio = windowWidth / 1000;
 
-    var textSizeRatioValue = textSizeRatio > 1 ? 1 : (textSizeRatio < 1 && textSizeRatio > 0.5 ? textSizeRatio : 0.5);
+    var textSizeRatioValue = textSizeRatio > 1 ? 1 : (textSizeRatio < 1 && textSizeRatio > 0.7 ? textSizeRatio : 0.7);
 
     textSize.style.setProperty('--textSizeRatioValue', textSizeRatioValue);
 }
@@ -35,14 +71,13 @@ textSizeFunction();
 window.addEventListener('resize', textSizeFunction);
 
 
-// Get the current scroll position
 
 var perspectiveText = document.querySelector(".perspective-text");
 
 function activateAnimation() {
     var perspectiveTextPosition = perspectiveText.offsetTop+400;
     var scrollPosition = window.scrollY;
-    console.log(perspectiveTextPosition);
+  
 
 
     if (scrollPosition >= perspectiveTextPosition) {
